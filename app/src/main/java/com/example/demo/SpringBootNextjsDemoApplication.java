2b56379f16822b31d5fb9325cac554a0f2567dc8
KurachiWeb <62194752+kurachiweb@users.noreply.github.com>
2025-11-05 18:39:31 +0900

    feature: install backend

---
+++ app/src/main/java/com/example/demo/SpringBootNextjsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNextjsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNextjsDemoApplication.class, args);
+	}
+
+}
